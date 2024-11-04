5e4ace05630b8c9297bf3872ecf294ba6687a0fa
LilyanGuedes <108057303+LilyanGuedes@users.noreply.github.com>
2024-11-03 21:26:46 -0300

    feat/student controller

--- src/main/java/com/lily/gym_management/controllers/StudentController.java
+++ src/main/java/com/lily/gym_management/controllers/StudentController.java
@@ -1,16 +1,59 @@
 package com.lily.gym_management.controllers;
-
+import com.lily.gym_management.entities.Gym;
+import com.lily.gym_management.entities.Student;
+import com.lily.gym_management.repositories.GymRepository;
+import com.lily.gym_management.useCases.FindAllStudentsUseCase;
+import com.lily.gym_management.useCases.RegisterStudentUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/students")
 public class StudentController {
 
-    @GetMapping
-    public ResponseEntity<String> getAllStudent() {
-        return ResponseEntity.ok("deu ok");
+    private final RegisterStudentUseCase registerStudentUseCase;
+
+    private final FindAllStudentsUseCase findAllStudentsUseCase;
+
+    @Autowired
+    private GymRepository gymRepository;
+
+    @Autowired
+    public StudentController(RegisterStudentUseCase registerStudentUseCase, FindAllStudentsUseCase findAllStudentsUseCase) {
+        this.registerStudentUseCase = registerStudentUseCase;
+        this.findAllStudentsUseCase = findAllStudentsUseCase;
     }
+
+    @PostMapping
+    public ResponseEntity<Student> registerStudent(@RequestBody StudentDto studentDto) {
+        Gym gym = findGymById(studentDto.gymId);
+        if (gym == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Student student = registerStudentUseCase.registerStudent(studentDto.name(), studentDto.dateOfBirth(), studentDto.paymentOnTime(), gym);
+        return ResponseEntity.ok(student);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findAllStudents() {
+        List<Student> students = findAllStudentsUseCase.execute();
+        return ResponseEntity.ok(students);
+    }
+
+    private Gym findGymById(UUID gymId) {
+        return gymRepository.findById(gymId).orElse(null);
+    }
+
+    public record StudentDto(
+            String name,
+            LocalDate dateOfBirth,
+            Boolean paymentOnTime,
+            UUID gymId
+    ) {}
 }
--- src/main/java/com/lily/gym_management/useCases/FindAllStudentsUseCase.java
+++ src/main/java/com/lily/gym_management/useCases/FindAllStudentsUseCase.java
@@ -3,9 +3,11 @@ package com.lily.gym_management.useCases;
 import com.lily.gym_management.entities.Student;
 import com.lily.gym_management.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FindAllStudentsUseCase {
 
     private final StudentRepository repository;
